abeac6bc2deb11d128ae3205dcfe7fdb32ddc82b
pathan17 <badhanpathan17@gmail.com>
2024-05-07 12:04:13 -0700

    firebaseuppdate

--- src/Components/ProfilePicture/ProfilePicture.jsx
+++ src/Components/ProfilePicture/ProfilePicture.jsx
@@ -1,28 +1,26 @@
 import { useEffect, useState } from "react";
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 
+const ProfilePicture = ({ imgId }) => {
+  let [ProfilePicture, setProfilePicture] = useState("");
+  // console.log(ProfilePicture)
+  const storage = getStorage();
+  const pictureRef = ref(storage, imgId);
 
-
-
-
-const ProfilePicture = ({imgId}) => {
-
-    let [ProfilePicture, setProfilePicture] = useState('')
-    // console.log(ProfilePicture)
-    const storage = getStorage();
-    const pictureRef = ref(storage, imgId)
-
-    useEffect(() => {
-        getDownloadURL(pictureRef)
-            .then((url) => { setProfilePicture(url) })
-            .catch((error) => { console.log(error) })
-    })
-    return (
-        <div>
-
-            <img className=" rounded-full " src={ProfilePicture} alt="" />
-        </div>
-    );
+  useEffect(() => {
+    getDownloadURL(pictureRef)
+      .then((url) => {
+        setProfilePicture(url);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [imgId]);
+  return (
+    <div>
+      <img className=" rounded-full " src={ProfilePicture} alt="" />
+    </div>
+  );
 };
 
 export default ProfilePicture;
